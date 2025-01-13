208978133eeb3d5dc7450a918c93bd3825f72aaa
Vinícius de Paula <manoseilacara123@gmail.com>
2025-01-13 11:59:43 -0300

    SubTotal e Total methods

--- src/main/java/com/vinisnzy/project/entities/OrderItem.java
+++ src/main/java/com/vinisnzy/project/entities/OrderItem.java
@@ -62,6 +62,10 @@ public class OrderItem implements Serializable {
         this.quantity = quantity;
     }
 
+    public Double getSubTotal() {
+        return price * quantity;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
