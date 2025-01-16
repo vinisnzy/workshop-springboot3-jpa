736e9594729a585b2da52db74171551a6be68fe3
Vinícius de Paula <manoseilacara123@gmail.com>
2025-01-16 11:32:03 -0300

    Exception handling - delete

---
+++ src/main/java/com/vinisnzy/project/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.vinisnzy.project.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
